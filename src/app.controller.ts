23210699a8c6363d5ab45ee9fb640ddc75985601
Deleanu Radu <radu_deleanu@outlook.com>
2023-08-24 20:33:40 +0300

    initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('api')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Post('register')
+  async register(
+    @Body('name') name: string,
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+
+  }
+}
